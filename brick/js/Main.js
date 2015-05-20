4983396b94ee973d93be408d552c1fed687c0a75
Daniel Law <djl1005@rit.edu>
2015-05-20 01:49:52 -0400

    Sprite for bomb drawing and locked to screen

--- brick/js/Main.js
+++ brick/js/Main.js
@@ -12,7 +12,55 @@ var mainScreen = function (game) {
     this.waveText = null;
     this.money = null;	//How much money you have
     this.moneyText = null;
-	this.costText = null;	//Cost of the currently selected tower
+    this.costText = null;	//Cost of the currently selected tower
+    this.bomb = {
+        x: 25,
+        y: 75,
+        r: 25,
+        damage : 5,
+        time: 5,
+        timer: 5,
+        vX: 0,
+        vY: 0,
+        sprite: undefined,
+
+        speed: 1,
+
+        update: function (dt) {
+            if (this.timer > 0) this.timer -= dt;
+            this.x += this.vX * dt;
+            this.y += this.vY * dt;
+
+            this.constarin();
+
+            this.sprite.position.x = this.x;
+            this.sprite.position.y = this.y;
+
+            this.vX = 0;
+            this.vY = 0;
+        },
+
+        constarin : function()
+        {
+            if (this.x < 25) this.x = 25;
+            if (this.x > 500 - 25) this.x = 500 - 25;
+            if (this.y < 75) this.y = 75;
+            if (this.y > 350 - 75) this.y = 350- 75;
+        },
+
+        fire: function ()
+        {
+            if (this.timer <= 0) {
+                this.timer = this.time;
+                return true;
+            }
+            return false;
+        },
+
+        colide : function(eX,eY, eR){
+            return (this.x - eX) * (this.x - eX) + (this.y - eY) * (this.y - eY) <= (this.r + er) * (this.r + er);
+        }
+    };
 }
 
 mainScreen.prototype = {
@@ -27,7 +75,8 @@ mainScreen.prototype = {
         game.load.image("towerB", "media/towerB.png", 50, 50);
         game.load.image("towerC", "media/towerC.png", 50, 50);
         game.load.image("towerD", "media/towerD.png", 50, 50);
-		game.load.image("cop", "media/cop.png", 50, 50);
+        game.load.image("cop", "media/cop.png", 50, 50);
+        game.load.image("bomb", "media/reticle.png", 50, 50);
         game.load.image("brick", "media/brick.png", 15 ,4);
         game.load.image("brickLaser", "media/BrickLaser.png", 500 ,4);
     },
@@ -43,15 +92,17 @@ mainScreen.prototype = {
         this.gameHeight = 350;
 
         this.tileArray = [];				// array with all game tiles
-        this.tileGroup; 				// group containing all tiles
+        this.tileGroup;                     // group containing all tiles
+
+        
 
         this.uiTop = null;	//base for the top ui
         this.uiBot = null;	//base for the bottom ui
-		this.towerASprite = null;	//towerA image
-		this.towerBSprite = null;	//towerB image
-		this.towerCSprite = null;	//towerC image
-		this.towerDSprite = null;	//towerD image
-		this.copSprite = null;		//cop image
+        this.towerASprite = null;	//towerA image
+        this.towerBSprite = null;	//towerB image
+        this.towerCSprite = null;	//towerC image
+        this.towerDSprite = null;	//towerD image
+        this.copSprite = null;		//cop image
 		
         this.frameArray = [];	//array containing the tower frames
         this.towerArray = [];
@@ -61,18 +112,18 @@ mainScreen.prototype = {
         this.bulletArray = [];
         this.bulletSpriteArray = [];
 		
-		this.wave = null;	//The current wave
-		this.waveEnemies = [];	//The enemies of the wave
-		this.maxSpawn = 10;		//The maximum amount of enemies to spawn at once
+        this.wave = null;	//The current wave
+        this.waveEnemies = [];	//The enemies of the wave
+        this.maxSpawn = 10;		//The maximum amount of enemies to spawn at once
 		
-		this.selectedTower = null;
+        this.selectedTower = null;
 		
         this.waveNumber = 1;
-		this.lastTime = Date.now();
-		this.lastTime = Date.now();
+        this.lastTime = Date.now();
+        this.lastTime = Date.now();
 		
         this.money = 100;
-		this.gainMoney = true;
+        this.gainMoney = true;
     },
 	
     create: function(){
@@ -94,16 +145,21 @@ mainScreen.prototype = {
         this.uiTop.scale.x = 0.5;
         this.uiTop.scale.y = 0.5;
 		
+
 		
         this.uiBot = game.add.sprite(0, this.gameHeight - this.tileSize, "uiBase");
         this.uiBot.scale.x = 0.5;
         this.uiBot.scale.y = 0.5;
 		
-		this.towerASprite = game.add.sprite(0, this.gameHeight - this.tileSize, "towerA");
-		this.towerBSprite = game.add.sprite(50, this.gameHeight - this.tileSize, "towerB");
-		this.towerBSprite = game.add.sprite(100, this.gameHeight - this.tileSize, "towerC");
-		this.towerBSprite = game.add.sprite(150, this.gameHeight - this.tileSize, "towerD");
-		this.copSprite = game.add.sprite(450, this.gameHeight - this.tileSize, "cop");
+        this.towerASprite = game.add.sprite(0, this.gameHeight - this.tileSize, "towerA");
+        this.towerBSprite = game.add.sprite(50, this.gameHeight - this.tileSize, "towerB");
+        this.towerBSprite = game.add.sprite(100, this.gameHeight - this.tileSize, "towerC");
+        this.towerBSprite = game.add.sprite(150, this.gameHeight - this.tileSize, "towerD");
+        this.copSprite = game.add.sprite(450, this.gameHeight - this.tileSize, "cop");
+
+        //bomb
+        this.bomb.sprite = game.add.sprite(this.bomb.x, this.bomb.y, "bomb");
+        this.bomb.sprite.anchor.setTo(0.5, 0.5);
 		
         //Frames for bottom ui
         for(var i = 0; i < this.fieldSizeCol; i++){
@@ -117,22 +173,26 @@ mainScreen.prototype = {
 		
         this.moneyText = game.add.text(100, this.tileSize / 4, "$" + this.money , { font: '20px Arial', fill: '#fff' });
 		
-		this.costText = game.add.text(300, this.tileSize / 4, "Tower Cost: ", { font: '20px Arial', fill: '#fff' });
+        this.costText = game.add.text(300, this.tileSize / 4, "Tower Cost: ", { font: '20px Arial', fill: '#fff' });
 
         game.input.onDown.add(this.click, this);
+
+        game.input.keyboard.addCallbacks(this, this.key);
 		
-		//Wave1
-		this.makeWave(10, 1);
+        //Wave1
+        this.makeWave(10, 1);
     },
 
     update: function() {
-		//Update time
-		var currentTime = Date.now();
-		var dt = currentTime - this.lastTime;
-		this.lastTime = currentTime;
+        //Update time
+        var currentTime = Date.now();
+        var dt = currentTime - this.lastTime;
+        this.lastTime = currentTime;
 	
-		//Damage towers and enemies
+        //Damage towers and enemies
         this.dealDamage(dt);
+
+        this.bomb.update(dt);
 		
         //Enemy
         for (var i = this.enemyArray.length -1; i >= 0; i--) {
@@ -140,74 +200,74 @@ mainScreen.prototype = {
             this.enemyArray[i].move(this.enemyArray, this.enemySpriteArray, i);
 	
             if (this.enemyArray[i].hp <= 0) {
-				if(this.gainMoney){
-					//Give the player some cash if they've earned it
-					this.money += this.enemyArray[i].reward;
-				}
+                if(this.gainMoney){
+                    //Give the player some cash if they've earned it
+                    this.money += this.enemyArray[i].reward;
+                }
                 //Kill the enemy
                 this.enemySpriteArray[i].kill();
                 var dead = this.enemyArray.indexOf(this.enemyArray[i]);
                 this.enemyArray.splice( dead, 1 );
                 this.enemySpriteArray.splice( dead, 1 );
             }
-			else if(this.enemyArray[i].x < 0){
-				//Take some money because the player is bad
-				this.money -= this.enemyArray[i].reward;
+            else if(this.enemyArray[i].x < 0){
+                //Take some money because the player is bad
+                this.money -= this.enemyArray[i].reward;
                 //Kill the enemy
                 this.enemySpriteArray[i].kill();
                 var dead = this.enemyArray.indexOf(this.enemyArray[i]);
                 this.enemyArray.splice( dead, 1 );
                 this.enemySpriteArray.splice( dead, 1 );
 				
-				//Lose if an enemy gets to the end when you have no money
-				if(this.money <= 0){
-					this.game.state.start('End');
-				}
-			}
+                //Lose if an enemy gets to the end when you have no money
+                if(this.money <= 0){
+                    this.game.state.start('End');
+                }
+            }
         }
 		
-		//If we called the cops, gain money for next kills
-		if(this.gainMoney == false)
-			this.gainMoney = true;
+        //If we called the cops, gain money for next kills
+        if(this.gainMoney == false)
+            this.gainMoney = true;
 		
-		//Towers
-		for (var i = 0; i < this.towerArray.length; i++){
-			if(this.towerArray[i].hp <= 0){
+        //Towers
+        for (var i = 0; i < this.towerArray.length; i++){
+            if(this.towerArray[i].hp <= 0){
                 //Kill the tower
-				this.tileArray[this.towerArray[i].arrayI][this.towerArray[i].arrayJ].hasTower = false;
+                this.tileArray[this.towerArray[i].arrayI][this.towerArray[i].arrayJ].hasTower = false;
                 this.towerSpriteArray[i].kill();
                 var dead = this.towerArray.indexOf(this.towerArray[i]);
                 this.towerArray.splice( dead, 1 );
                 this.towerSpriteArray.splice( dead, 1 );
-			}
-		}
+            }
+        }
 
         //Bullets
-		for (var i = 0; i < this.bulletArray.length; i++) {
-		    this.bulletArray[i].move(this.bulletArray, this.bulletSpriteArray, i);
-			if(this.bulletArray[i].sprite.position.x > this.gameWidth){
-			    this.bulletSpriteArray[i].kill();
-			    var dead = this.bulletArray.indexOf(this.bulletArray[i]);
-			    this.bulletArray.splice(dead, 1);
-			    this.bulletSpriteArray.splice(dead, 1);
-			}
-		}
+        for (var i = 0; i < this.bulletArray.length; i++) {
+            this.bulletArray[i].move(this.bulletArray, this.bulletSpriteArray, i);
+            if(this.bulletArray[i].sprite.position.x > this.gameWidth){
+                this.bulletSpriteArray[i].kill();
+                var dead = this.bulletArray.indexOf(this.bulletArray[i]);
+                this.bulletArray.splice(dead, 1);
+                this.bulletSpriteArray.splice(dead, 1);
+            }
+        }
 		
         this.wave.update(this.enemyArray, this.enemySpriteArray);
 		
-		//Spawn the next wave
+        //Spawn the next wave
         if(this.enemyArray.length == 0){
-			this.waveNumber += 1;
-			//Don't spawn more than the maximum amount to prevent lag
-			if(this.waveNumber <= this.maxSpawn)
-				this.makeWave(10 * this.waveNumber, this.waveNumber);
-			else
-				this.makeWave(10 * this.waveNumber, this.maxSpawn);
+            this.waveNumber += 1;
+            //Don't spawn more than the maximum amount to prevent lag
+            if(this.waveNumber <= this.maxSpawn)
+                this.makeWave(10 * this.waveNumber, this.waveNumber);
+            else
+                this.makeWave(10 * this.waveNumber, this.maxSpawn);
         }
 		
         //Update  Text
         this.moneyText.setText("$" + this.money);
-		this.waveText.setText("Wave " + this.waveNumber);
+        this.waveText.setText("Wave " + this.waveNumber);
 		
     },
 	
@@ -228,6 +288,13 @@ mainScreen.prototype = {
         }
     },
 
+    key: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.W)) this.bomb.vY -= this.bomb.speed;
+        if( game.input.keyboard.isDown(Phaser.Keyboard.S)) this.bomb.vY += this.bomb.speed;
+        if(game.input.keyboard.isDown(Phaser.Keyboard.A)) this.bomb.vX -= this.bomb.speed;
+        if (game.input.keyboard.isDown(Phaser.Keyboard.D)) this.bomb.vX += this.bomb.speed;
+    },
+
     //Selects a tower to place
     buyTower: function(button){
         //Testing to make sure its selecting the right frame
@@ -235,31 +302,31 @@ mainScreen.prototype = {
 		
         //Placing logic goes here
         //Use button to determine which was pressed
-		this.selectedTower = button.num;
+        this.selectedTower = button.num;
 		
-		if(button.num == 0)
-			this.costText.setText("Tower Cost: 10");
-		if(button.num == 1)
-			this.costText.setText("Tower Cost: 30");
-		if(button.num == 2)
-			this.costText.setText("Tower Cost: 20");
-		if(button.num == 3)
-			this.costText.setText("Tower Cost: 100");
+        if(button.num == 0)
+            this.costText.setText("Tower Cost: 10");
+        if(button.num == 1)
+            this.costText.setText("Tower Cost: 30");
+        if(button.num == 2)
+            this.costText.setText("Tower Cost: 20");
+        if(button.num == 3)
+            this.costText.setText("Tower Cost: 100");
 			
-		//Cop powerup
-		if(button.num == 9){
-			//Do we have enough money
-			if(this.money >= 100){
-				//Spend money and kill enemies
-				this.money -= 100;
-				for(var i = 0; i < this.enemyArray.length; i++){
-					this.enemyArray[i].hp -= 100;
-				}
+        //Cop powerup
+        if(button.num == 9){
+            //Do we have enough money
+            if(this.money >= 100){
+                //Spend money and kill enemies
+                this.money -= 100;
+                for(var i = 0; i < this.enemyArray.length; i++){
+                    this.enemyArray[i].hp -= 100;
+                }
 				
-				//No money for cops
-				this.gainMoney = false;
-			}
-		}
+                //No money for cops
+                this.gainMoney = false;
+            }
+        }
     },
 	
     //Add enemies to a wave
@@ -284,12 +351,12 @@ mainScreen.prototype = {
                     tempEnemy = new Enemy(550, spawnLoc, 20, 1, "punkA", 500, 1);
                     break;
 
-                //fast enemy
+                    //fast enemy
                 case 3:
                     tempEnemy = new Enemy(550, spawnLoc, 10, 1, "punkB", 200, 2);
                     break;
 
-                //fat enemy
+                    //fat enemy
                 case 4:
                     tempEnemy = new Enemy(550, spawnLoc, 50, 3, "punkC", 1000, 0.5);
                     break;
@@ -303,58 +370,58 @@ mainScreen.prototype = {
         var x = Math.floor(_x/this.tileSize) * this.tileSize;
         var y = Math.floor(_y / this.tileSize) * this.tileSize;
 		
-		//Check what tower is selected
-		var tempTower = null;
-		//Melee
-		if(this.selectedTower == 0){
-			tempTower = new Tower(x, y, 30, 1, 'towerA', 10, 500, i, j);
-		}
-		//Ranged
-		if(this.selectedTower == 1){
-			tempTower = new Tower(x, y, 10, 5, 'towerB', 30, 1000, i, j);
-		}
-		//Blockade
-		if(this.selectedTower == 2){
-			tempTower = new Tower(x, y, 60, 0, 'towerC', 20, 1000, i, j);
-		}
-		//Ranged
-		if(this.selectedTower == 3){
-			tempTower = new Tower(x, y, 30, 30, 'towerD', 100, 5000, i, j);
-		}
+        //Check what tower is selected
+        var tempTower = null;
+        //Melee
+        if(this.selectedTower == 0){
+            tempTower = new Tower(x, y, 30, 1, 'towerA', 10, 500, i, j);
+        }
+        //Ranged
+        if(this.selectedTower == 1){
+            tempTower = new Tower(x, y, 10, 5, 'towerB', 30, 1000, i, j);
+        }
+        //Blockade
+        if(this.selectedTower == 2){
+            tempTower = new Tower(x, y, 60, 0, 'towerC', 20, 1000, i, j);
+        }
+        //Ranged
+        if(this.selectedTower == 3){
+            tempTower = new Tower(x, y, 30, 30, 'towerD', 100, 5000, i, j);
+        }
 		
 		
-		//Spawn the tower and subtract money
-		if(tempTower != null){
-			if(this.money >= tempTower.cost){
-				this.towerArray.push(tempTower);
-				this.towerSpriteArray.push(tempTower.sprite);
-				this.money -= tempTower.cost;
-				this.tileArray[i][j].hasTower = true;
-			}
-			else
-				tempTower.sprite.kill();	
-		}
-	},
+        //Spawn the tower and subtract money
+        if(tempTower != null){
+            if(this.money >= tempTower.cost){
+                this.towerArray.push(tempTower);
+                this.towerSpriteArray.push(tempTower.sprite);
+                this.money -= tempTower.cost;
+                this.tileArray[i][j].hasTower = true;
+            }
+            else
+                tempTower.sprite.kill();	
+        }
+    },
 
-	//Deal damage to enemies and towers
-	dealDamage: function(dt){
-		//Enemies
-		for(var i = 0; i < this.towerArray.length; i++){
-			this.towerArray[i].update(this.enemyArray, this.bulletArray, this.bulletSpriteArray, dt);
-		}
-		//Towers
-		for(var i = 0; i < this.enemyArray.length; i++){
-		    this.enemyArray[i].update(this.towerArray, this.bulletArray, this.bulletSpriteArray, dt);
-		}
-	},
+    //Deal damage to enemies and towers
+    dealDamage: function(dt){
+        //Enemies
+        for(var i = 0; i < this.towerArray.length; i++){
+            this.towerArray[i].update(this.enemyArray, this.bulletArray, this.bulletSpriteArray, dt);
+        }
+        //Towers
+        for(var i = 0; i < this.enemyArray.length; i++){
+            this.enemyArray[i].update(this.towerArray, this.bulletArray, this.bulletSpriteArray, dt);
+        }
+    },
 	
-	//Makes a new wave, allows you to specify amount of enemies in the wave, as well as how many spawn at once
-	makeWave: function(total, perSpawn){
-		this.wave = null;
-		this.waveEnemies = [];
-		this.spawnEnemy(total);
-		var tempWave = new Wave(this.waveEnemies, 2000, perSpawn, total);
-		this.wave = tempWave;
-	}
+    //Makes a new wave, allows you to specify amount of enemies in the wave, as well as how many spawn at once
+    makeWave: function(total, perSpawn){
+        this.wave = null;
+        this.waveEnemies = [];
+        this.spawnEnemy(total);
+        var tempWave = new Wave(this.waveEnemies, 2000, perSpawn, total);
+        this.wave = tempWave;
+    }
 
 };
